c7cea9835a5748cf77c46bf86ce0f921a2caecce
Sushi22 <sushmitajoshi22@gmail.com>
2020-08-26 20:08:34 +0530

    added gunicorn

--- hr_api/settings.py
+++ hr_api/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost','.herokuapp.com']
+ALLOWED_HOSTS = ['localhost','https://django-hrapi.herokuapp.com/']
 
 
 # Application definition
