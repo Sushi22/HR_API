f9f8f11e445858ed2affad00368f0cdd3e5d4548
Sushi22 <sushmitajoshi22@gmail.com>
2020-08-26 19:54:59 +0530

    wsgi.py changed

--- Procfile
+++ Procfile
@@ -1,3 +1,2 @@
-web: gunicorn hr_api.wsgi:application --log-file - --log-level debug
-python manage.py collectstatic --noinput
-manage.py migrate
+web: gunicorn hr_api.wsgi:application --log-file - 
+
--- hr_api/settings.py
+++ hr_api/settings.py
@@ -132,6 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT=
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
